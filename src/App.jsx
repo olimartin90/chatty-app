5c2da86e6dcdc291808706ea22a25c6b37d9f36a
Olivier Martin <olimartin90@gmail.com>
2018-07-10 17:19:56 +0000

    element component and set the initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar currentUser={this.state.currentUser.name} />
+      </div>
     );
   }
 }
